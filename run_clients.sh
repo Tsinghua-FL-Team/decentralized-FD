0ee0cb8ec719df490c699c82cafa2396798749c1
Usama Zafar <usama-zafar@outlook.com>
2023-08-28 14:58:15 +0200

    Revert "slurm deployment configurations"
    This reverts commit bab41c65ca74c04380f88aae1d8b5bdc8988cf35.

--- run_clients.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python src/run_fl_clients.py --total_clients=5 --num_clients=5 --start_cid=0 --config_file="src/configs/exp_configs.yaml"
--- run_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python src/run_fl_server.py --config_file="src/configs/exp_configs.yaml"

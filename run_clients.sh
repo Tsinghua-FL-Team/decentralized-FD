15cb0b6b3fbcf370c204c0b191ecc754b1f03f1d
Usama Zafar <usama-zafar@outlook.com>
2023-08-28 15:08:24 +0200

    chage to param

--- run_clients.sh
+++ run_clients.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python src/run_fl_clients.py --total_clients=5 --num_clients=5 --start_cid=0 --config_file="src/configs/exp_configs.yaml"
+python src/run_fl_clients.py --total_clients=5 --num_clients=5 --start_cid=0 --max_gpus=4 --config_file="src/configs/exp_configs.yaml"

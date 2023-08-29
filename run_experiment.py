7823ec5253bf82ee990b890db6d889f1e08fbcf3
Usama Zafar <usama-zafar@outlook.com>
2023-08-29 10:13:07 +0200

    up

--- run_experiment.py
+++ run_experiment.py
@@ -1,6 +1,5 @@
 import argparse
 
-
 def main():
     parser = argparse.ArgumentParser(description="Flower")
     parser.add_argument(
@@ -15,10 +14,17 @@ def main():
         required=True,
         help="Current host node (no default)",
     )
+    parser.add_argument(
+        "--configs_file",
+        type=str,
+        required=True,
+        help="Experiment configurations file (no default)",
+    )
     args = parser.parse_args()
 
     print(f"Allocated: {args.allocated_hosts}")
     print(f"Current: {args.current_host}")
+    print(f"Configs: {args.configs_file}")
 
 
 if __name__=="__main__":

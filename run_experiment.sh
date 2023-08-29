298375c249fd1ddcfb06f7e1abac436fcf067be4
Usama Zafar <usama-zafar@outlook.com>
2023-08-29 10:36:46 +0200

    Streamlined running

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
 
-hostnames=$1
-configs_file=$2
+# Activate anaconda environment for flower framework
+source /home/${USER}/.bashrc
+conda activate fl_flower
+
+# Check for current host name
 current_host=$(hostname)
 
+# Run the python based experiment manager
+python run_experiment.py --allocated_hosts=$1 --current_host=$current_host --configs_file=$2
 
-echo $hostnames
-echo $current_host
-
-
-
-#srun --exclusive --nodelist=s177 --output="server_output" run_server.sh --server_address="0.0.0.0:59999" --config_file="src/configs/exp_configs.yaml" &
-#srun --exclusive --output="client_output_m1" run_clients.sh --server_address="s177:59999" --total_clients=5 --num_clients=2 --start_cid=0 --config_file="src/configs/exp_configs.yaml" &
-#srun --exclusive --output="client_output_m2" run_clients.sh --server_address="s177:59999" --total_clients=5 --num_clients=2 --start_cid=2 --config_file="src/configs/exp_configs.yaml" &
-#srun --exclusive --output="client_output_m3" run_clients.sh --server_address="s177:59999" --total_clients=5 --num_clients=1 --start_cid=4 --config_file="src/configs/exp_configs.yaml" &

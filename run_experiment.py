203c6b6dbea3fcc1c07064774ef1728cc70ca90a
Usama Zafar <usama-zafar@outlook.com>
2023-08-29 11:03:45 +0200

    Finalizing Changes

--- run_experiment.py
+++ run_experiment.py
@@ -1,4 +1,6 @@
+import subprocess
 import argparse
+import re
 
 def main():
     parser = argparse.ArgumentParser(description="Flower")
@@ -22,10 +24,30 @@ def main():
     )
     args = parser.parse_args()
 
-    print(f"Allocated: {args.allocated_hosts}")
-    print(f"Current: {args.current_host}")
+    allocated_hosts = [f"{args.allocated_hosts[0]}{s}" for s in re.findall(r'\d+', args.allocated_hosts)]
+    current_host = [f"{args.current_host[0]}{s}" for s in re.findall(r'\d+', args.current_host)]
+    
+    print(f"Allocated: {allocated_hosts}")
+    print(f"Current: {current_host}")
     print(f"Configs: {args.configs_file}")
 
+    if len(current_host) > 1:
+        print("Something Went Horribly Wrong!!!")
+        return
+    else:
+
+        if current_host[0] == allocated_hosts[0]:
+            # need to run server on the first
+            # allocated node of the cluster
+            server_call = f"python src/run_fl_server.py --server_address='0.0.0.0:59999' --config_file={args.configs_file}"
+            print(f"Client Call: {server_call}")
+            subprocess.call([server_call])
+        else:
+            # need to run client instance on
+            # all other allocated nodes
+            client_call = f"python src/run_fl_clients.py --server_address='{allocated_hosts[0]}:59999' --total_clients={len(allocated_hosts)} --num_clients=1 --start_cid={allocated_hosts.index(current_host[0])-1} --config_file={args.configs_file}"
+            print(f"Client Call: {client_call}")
+            subprocess.call([client_call])
 
 if __name__=="__main__":
     main()

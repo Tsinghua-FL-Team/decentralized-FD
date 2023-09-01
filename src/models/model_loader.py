428462776420cfeef97b1ece381d76b0ee61f322
Usama Zafar <usama-zafar@outlook.com>
2023-09-01 18:07:53 +0200

    designing and running experiments

--- src/models/model_loader.py
+++ src/models/model_loader.py
@@ -2,19 +2,25 @@
 
 def load_model(model_configs: dict):
     
-    assert model_configs["MODEL_NAME"] in ["SIMPLE-CNN", "SIMPLE-MLP", "LENET-MNIST", "LENET-CIFAR"], f"Invalid model {model_configs['MODEL_NAME']} requested."
+    assert model_configs["MODEL_NAME"] in ["SIMPLE-CNN", "SIMPLE-MLP", "LENET-MNIST", "LENET-CIFAR", "RESNET8-CIFAR", "RESNET18-CIFAR"], f"Invalid model {model_configs['MODEL_NAME']} requested."
 
     if model_configs["MODEL_NAME"] == "SIMPLE-MLP":
         from .simple_mlp import Net
-        return Net #(num_classes = model_configs["NUM_CLASSES"])
+        return Net 
     elif model_configs["MODEL_NAME"] == "SIMPLE-CNN":
         from .simple_cnn import Net
-        return Net #(num_classes = model_configs["NUM_CLASSES"])
+        return Net 
     elif model_configs["MODEL_NAME"] == "LENET-MNIST":
         from .lenet_mnist import Net
-        return Net #(num_classes = model_configs["NUM_CLASSES"])
+        return Net 
     elif model_configs["MODEL_NAME"] == "LENET-CIFAR":
         from .lenet_cifar import Net
-        return Net #(num_classes = model_configs["NUM_CLASSES"])
+        return Net 
+    elif model_configs["MODEL_NAME"] == "RESNET8-CIFAR":
+        from .resnet8_cifar import Net
+        return Net 
+    elif model_configs["MODEL_NAME"] == "RESNET18-CIFAR":
+        from .resnet18_cifar import Net
+        return Net 
     else:
         raise Exception(f"Invalid model {model_configs['MODEL_NAME']} requested.")
---
+++ src/models/resnet8_cifar.py
@@ -0,0 +1,28 @@
+"""Implementation of a simple multilayer perceptron network."""
+
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+from torchvision.models.resnet import ResNet, BasicBlock
+
+import flwr as fl
+
+class Net(ResNet):
+    """Multilayer percenptron (MLP) network."""
+    def __init__(self, num_classes) -> None:
+        super(Net, self).__init__(block=BasicBlock, layers=[1,1,1,1], num_classes=num_classes)
+        self._num_classes = num_classes
+
+    def get_weights(self) -> fl.common.NDArrays:
+        """Get model weights as a list of NumPy ndarrays."""
+        return [val.cpu().numpy() for _, val in self.state_dict().items()]
+
+    def set_weights(self, weights: fl.common.NDArrays) -> None:
+        """Set model weights from a list of NumPy ndarrays."""
+        state_dict = OrderedDict(
+            {k: torch.Tensor(v) for k, v in zip(self.state_dict().keys(), weights)}
+        )
+        self.load_state_dict(state_dict, strict=True)

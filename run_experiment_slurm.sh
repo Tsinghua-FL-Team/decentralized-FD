2c753007e9399d07fb9d2a227903dc41d8ff44ab
Usama Zafar <usama-zafar@outlook.com>
2023-10-11 09:09:38 +0200

    slurm runner

---
+++ run_experiment_slurm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#SBATCH -A NAISS2023-22-904 -p alvis
+#SBATCH --gpus=4
+#SBATCH --gpus-per-node=T4:4
+#SBATCH --nodes=1 --ntasks=1
+#SBATCH -t 0-00:01:00 
+#SBATCH -J cifar10_modified_run
+#SBATCH --output cifar10_modified_run.txt
+
+hostnames=$SLURM_NODELIST
+echo "Fetched Hostnames are $hostnames"
+wait
+
+apptainer exec --nv /mimer/NOBACKUP/groups/naiss2023-22-904/env_containers/flwr14_env.sif python run_experiment_slurm.py --num_gpus=4 --configs_path="current_experiments/"

9b46b8c4247d35ee39f9ae1361c7cef9f32fbd8c
Usama Zafar <usama-zafar@outlook.com>
2023-10-16 09:32:33 +0200

    squeezenet for CIFAR10 dataset

---
+++ src/models/squenet_cifar10.py
@@ -0,0 +1,32 @@
+"""Implementation of a simple multilayer perceptron network."""
+
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+from torchvision.models import SqueezeNet
+
+import flwr as fl
+
+class Net(SqueezeNet):
+    """Multilayer percenptron (MLP) network."""
+    def __init__(self, num_classes) -> None:
+        super(Net, self).__init__(num_classes=num_classes)
+        self._num_classes = num_classes
+
+    def get_weights(self) -> fl.common.NDArrays:
+        """Get model weights as a list of NumPy ndarrays."""
+        return [val.cpu().numpy() for _, val in self.state_dict().items()]
+
+    def set_weights(self, weights: fl.common.NDArrays) -> None:
+        """Set model weights from a list of NumPy ndarrays."""
+        state_dict = OrderedDict(
+            {k: torch.Tensor(v) for k, v in zip(self.state_dict().keys(), weights)}
+        )
+        self.load_state_dict(state_dict, strict=True)
+
+if __name__=="__main__":
+    model = Net(10)
+        

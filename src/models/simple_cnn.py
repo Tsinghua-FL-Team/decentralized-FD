9a7653b426772b5011c4349a46ddeb96113d373d
Usama Zafar <usama-zafar@outlook.com>
2023-10-16 12:59:40 +0200

    bug removal

--- src/models/simple_cnn.py
+++ src/models/simple_cnn.py
@@ -14,7 +14,7 @@ class Net(nn.Module):
     """Convolutional neural network."""
     def __init__(self, num_classes) -> None:
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(1, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
